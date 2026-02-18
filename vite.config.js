1111b0efdb690a360005ccc0435d7c2f353ab648
KirkHietpas <hietpaskirk@gmail.com>
2026-02-18 10:17:45 -0800

    fix: deploying

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Required for GitHub Pages - repo: Kirk_Hietpas_Portfolio
+  base: '/Kirk_Hietpas_Portfolio/',
 })
